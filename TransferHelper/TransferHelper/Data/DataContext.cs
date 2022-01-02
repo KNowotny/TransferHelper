81fdbc808ad493c03a27e2c80924e89f17ccd662
JakubN <nowotny.kuba@gmail.com>
2022-01-02 17:20:16 +0100

    DataContext created

---
+++ TransferHelper/TransferHelper/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TransferHelper.Models;
+
+namespace TransferHelper.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<PlayerPosition> PlayerPositions { get; set; }
+    }
+}
