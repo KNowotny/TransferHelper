d3694e0952e1a595be28bbe63298bd9e6a18fccc
JakubN <nowotny.kuba@gmail.com>
2022-01-02 17:07:19 +0100

    Player Model added

---
+++ TransferHelper/TransferHelper/Models/Player.cs
@@ -0,0 +1,13 @@
+﻿namespace TransferHelper.Models
+{
+    public class Player
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Surname { get; set; } = string.Empty;
+        public DateTime Birthdate { get; set; }
+        public Guid PositionId { get; set; }
+        public PlayerPosition? PositionName { get; set; }
+
+    }
+}
