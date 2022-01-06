4147140962f2ca37683dfa84a6d055a40cbff2e1
JakubN <nowotny.kuba@gmail.com>
2022-01-06 15:59:46 +0100

    Show-player component created

--- TransferHelperApp/src/app/app.module.ts
+++ TransferHelperApp/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlayerComponent } from './player/player.component';
+import { ShowPlayerComponent } from './player/show-player/show-player.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlayerComponent
+    PlayerComponent,
+    ShowPlayerComponent
   ],
   imports: [
     BrowserModule,
---
+++ TransferHelperApp/src/app/player/show-player/show-player.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-player',
+  templateUrl: './show-player.component.html',
+  styleUrls: ['./show-player.component.scss']
+})
+export class ShowPlayerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
