980fa9a6b982ad4d21027cb61f2d552650ebc47f
JakubN <nowotny.kuba@gmail.com>
2022-01-06 17:40:35 +0100

    Methods added in API service

--- TransferHelperApp/src/app/player-api.service.ts
+++ TransferHelperApp/src/app/player-api.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlayerApiService {
 
-  constructor() { }
+  readonly playerAPIUrl = "https://localhost:7228/api";
+
+  constructor(private http:HttpClient) { }
+
+  getPlayerList():Observable<any[]> {
+    return this.http.get<any>(this.playerAPIUrl + 'players');
+  }
+  
+  addPlayer(data:any) {
+    return this.http.post(this.playerAPIUrl + '/players', data);
+  }
+
+  deletePlayer(id:number|string){
+     return this.http.delete(this.playerAPIUrl + `/players/${id}`)
+  }
 }
