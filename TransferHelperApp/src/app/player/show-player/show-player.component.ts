64ff9f06f0496a7cd530687ca336dbe89430d68f
JakubN <nowotny.kuba@gmail.com>
2022-01-18 19:13:08 +0100

    tr fixed, .vs files removed and gitignore changed

--- TransferHelperApp/src/app/player/show-player/show-player.component.ts
+++ TransferHelperApp/src/app/player/show-player/show-player.component.ts
@@ -1,4 +1,3 @@
-import { isNgTemplate } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ApiService } from 'src/app/api.service';
